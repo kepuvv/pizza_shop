7030df3e2c10e5757589abed0b1878817c274fc6
Kepuvv <renard.ru@gmail.com>
2017-07-21 17:11:43 +0300

    java () hi from function

--- public/app.js
+++ public/app.js
@@ -1,10 +1,11 @@
 function something()
 	{
-		var x = window.localStorage.getItem('bbb');
 
-		x = x * 1 + 1;
-
-		window.localStorage.setItem('bbb', x);
-
-		alert (x);
+		alert ('hello');
 	}
+
+function add_to_cart()
+	{
+		alert('Hi from function');
+	}
+	
