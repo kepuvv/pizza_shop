74dc4a12d6ebde5154466330ea8a60bcbbddb7f6
kepuvv <renard.ru@gmail.com>
2017-07-25 19:50:41 +0300

    restore localStorage

--- public/app.js
+++ public/app.js
@@ -1,7 +1,9 @@
 function something()
 	{
-
-		alert ('hello');
+		var x = window.localStorage.getItem('bbb');
+        x = x * 1 + 1;
+        window.localStorage.setItem('bbb', x);
+		alert (x);
 	}
 
 function add_to_cart()
